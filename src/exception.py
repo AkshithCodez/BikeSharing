a551872fee1d56c06708d2f1e05ed02736b7dfaa
AkshithCodez <akshithreddy1318@gmail.com>
2025-09-21 08:42:09 +0530

    fixed errors

--- src/exception.py
+++ src/exception.py
@@ -4,10 +4,16 @@ import sys
 from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
-    _,_,exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name, exc_tb.tb_lineno, str(error))
+    _, _, exc_tb = error_detail.exc_info()
+    # Check if the traceback object exists
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        error_message = "Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format(
+            file_name, exc_tb.tb_lineno, str(error)
+        )
+    else:
+        # If no traceback, just return the error message
+        error_message = str(error)
 
     return error_message
 
@@ -15,6 +21,6 @@ class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
-    
+
     def __str__(self):
         return self.error_message
